3a0ac4b3a78aa5f824e094cc6b841e23a4b865cd
Aaron Martin <martina3203@gmail.com>
2015-05-13 17:00:16 -0400

    Working on Database design.

--- runnerdatabase.h
+++ runnerdatabase.h
@@ -3,13 +3,24 @@
 
 #include <QString>
 #include <QtSql>
+#include <QDebug>
+#include "Athlete.h"
+#include "RunningEvent.h"
+#include "RunningTime.h"
 
 class RunnerDatabase
 {
 public:
     RunnerDatabase();
+    ~RunnerDatabase();
+    addAthlete(Athlete);
+    addEvent(RunningEvent);
 private:
     QSqlDatabase DefaultDatabase;
+
+    //These are the strings used for default table names and column names
+    const QString RUNNER_TABLE_NAME = "RUNNERS";
+    const QString EVENT_TABLE_NAME = "EVENTS";
 };
 
 #endif // RUNNERDATABASE_H
